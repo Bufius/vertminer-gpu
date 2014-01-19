90c175d3e6a3d705788c0dd3ed54c23482a7674a
Bufius <helmut.ibiza@gmail.com>
2014-01-19 22:40:57 +0100

    backport fixes from cgminer, changed version number to 0.5.0

--- miner.h
+++ miner.h
@@ -1353,7 +1353,7 @@ struct pool {
 	char *nonce1;
 	unsigned char *nonce1bin;
 	size_t n1_len;
-	uint32_t nonce2;
+	uint64_t nonce2;
 	int n2size;
 	char *sessionid;
 	bool has_stratum;
@@ -1432,7 +1432,7 @@ struct work {
 
 	bool		stratum;
 	char 		*job_id;
-	uint32_t	nonce2;
+	uint64_t	nonce2;
 	size_t		nonce2_len;
 	char		*ntime;
 	double		sdiff;
